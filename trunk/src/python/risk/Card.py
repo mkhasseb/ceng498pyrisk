2eafe8b181a53516a06825e9d8537421f7cce3a4
cihancimen83 <cihancimen83@b637e66d-a63f-ac06-77cf-f84302a73d98>
2011-03-31 16:20:39 +0000

    Initial classes

---
+++ trunk/src/python/risk/Card.py
@@ -0,0 +1,17 @@
+'''
+Created on 2011 3 31
+
+@author: cihancimen
+'''
+
+class Card(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ trunk/src/python/risk/Goal.py
@@ -0,0 +1,17 @@
+'''
+Created on 2011 3 31
+
+@author: cihancimen
+'''
+
+class Goal(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
