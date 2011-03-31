b1532daf7cf15a2db503d1547c266f6e4050f8a5
cihancimen83 <cihancimen83@b637e66d-a63f-ac06-77cf-f84302a73d98>
2011-03-31 16:20:39 +0000

    Initial classes

---
+++ src/python/risk/Player.py
@@ -0,0 +1,17 @@
+'''
+Created on 2011 3 31
+
+@author: cihancimen
+'''
+
+class Player(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
