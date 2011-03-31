1e1b1f3df77fdecf7ae9c9a8d37fa6b3f4d8d7cd
cihancimen83 <cihancimen83@b637e66d-a63f-ac06-77cf-f84302a73d98>
2011-03-31 17:45:11 +0000

    Initial package config. Card and Goal implementation started.

--- trunk/src/python/risk/Card.py
+++ trunk/src/python/risk/Card.py
@@ -3,15 +3,28 @@ Created on 2011 3 31
 
 @author: cihancimen
 '''
+import uuid
+
 
 class Card(object):
     '''
     classdocs
     '''
+    type_infantry = "infantry"
+    type_cavalry = "cavalry"
+    type_artillery = "artillery"
+    type_wild = "wild"
+    type_tip = "tip"
 
-
-    def __init__(self):
+    def __init__(self, type, territory=None, tip=None):
         '''
         Constructor
         '''
+        self.id = uuid.uuid4()
+        self.type = type;
+        if(territory):
+            self.territory = territory
+        if(tip):
+            self.tip = tip
+    
         
--- trunk/src/python/risk/Goal.py
+++ trunk/src/python/risk/Goal.py
@@ -4,14 +4,62 @@ Created on 2011 3 31
 @author: cihancimen
 '''
 
+class GoalFactory(object):
+    verbose_occupy= "Occupy 24 Territories of your choice."
+    verbose_conquer = "Conquer 18 Territories of your choice and Occupy each with at least 2 Armies."
+    verbose_continent = "Conquer the Continents of"
+    verbose_eliminate = "Destroy all"
+    def __init__(self):
+        pass
+    @staticmethod
+    def createOccupy():
+        return Goal(Goal.type_occupy, GoalFactory.verbose_occupy)
+    @staticmethod
+    def createConquer():
+        return Goal(Goal.type_conquer, GoalFactory.verbose_conquer)
+    @staticmethod
+    def createConquerContinent(continents):
+        verbose = GoalFactory.verbose_conquer+" " + continents[0].name
+        verbose += " and " + continents[2].name
+        return Goal(Goal.type_continent, verbose, continents=continents)
+    @staticmethod
+    def createEliminate(color):
+        alternate = GoalFactory.createOccupy()
+        verbose = GoalFactory.verbose_continent + " " + color + " troops." + alternate.verbose  
+        return Goal(Goal.type_eliminate, verbose, color=color, alternate=alternate)
+    
 class Goal(object):
     '''
     classdocs
+    All of the goals are listed below
+    Occupy 24 Territories of your choice.
+    Conquer the Continents of North America and Africa.
+    Conquer the Continents of North America and Australia.
+    Conquer the Continents of Asia and Africa.
+    Conquer the Continents of Asia and South America.
+    Destroy all Black troops. If yours are the Black Troops, then: Occupy 24 Territories of your choice.
+    Destroy all Gray troops. If yours are the Gray Troops, then: Occupy 24 Territories of your choice.
+    Destroy all Green troops. If yours are the Green Troops, then: Occupy 24 Territories of your choice.
+    Destroy all Yellow troops. If yours are the Yellow Troops, then: Occupy 24 Territories of your choice.
+    Destroy all Red troops. If yours are the Red Troops, then: Occupy 24 Territories of your choice.    
+    Destroy all Blue troops. If yours are the Blue Troops, then: Occupy 24 Territories of your choice.
+    Conquer 18 Territories of your choice and Occupy each with at least 2 Armies.
     '''
+    type_eliminate = "eliminate"
+    type_continent = "continent"
+    type_occupy="occupy" #24 Territories
+    type_conquer ="conquer" #18 Territory at least two armies
 
-
-    def __init__(self):
+    def __init__(self, type, verbose, continents=None, color =None, alternate = None):
         '''
-        Constructor
+        Generic Constructor
+        
         '''
+        self.type = type
+        self.verbose = verbose
+        if(continents):
+            self.continents = continents
+        if(color):
+            self.color = color
+            self.alternate = alternate
         
