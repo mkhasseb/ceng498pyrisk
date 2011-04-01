6748df3309d1f8142352263dd70fdb19dd956732
ozgurcolpankan <ozgurcolpankan@gmail.com>
2011-04-01 13:42:43 +0000

    placeArmies, ListCommand finished

---
+++ src/python/risk/command/ListCommand.py
@@ -0,0 +1,17 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="ozgur"
+__date__ ="$Apr 1, 2011 3:33:37 PM$"
+
+
+class ListCommand(object):
+    '''
+    classdocs
+    '''
+    def __init__(self, verbose):
+        '''
+        Constructor
+        '''
+        self.verbose = verbose
+        
