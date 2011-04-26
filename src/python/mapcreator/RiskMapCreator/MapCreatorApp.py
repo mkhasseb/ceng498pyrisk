c2acb67868d8d7e34be36b8b3da44e8ee35ce6ca
cihancimen83 <cihancimen83@b637e66d-a63f-ac06-77cf-f84302a73d98>
2011-04-26 19:26:57 +0000

    Map creator continents.

--- src/python/mapcreator/RiskMapCreator/MapCreatorApp.py
+++ src/python/mapcreator/RiskMapCreator/MapCreatorApp.py
@@ -11,6 +11,7 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from PyQt4.pyqtconfig import QtGuiModuleMakefile
 from mainwindow import Ui_MainWindow
+from shutil import copyfile
 
 class Region(object):
     def __init__(self, name):
@@ -20,6 +21,7 @@ class Region(object):
         self.done = False
         self.itemp = None
         self.neighbours = []
+        self.continent= None
     def addPoint(self, point):
         self.polygon.insert(self.i - 1, point)
         self.polygon.insert(self.i, self.polygon.at(0))
@@ -39,20 +41,29 @@ class MapCreatorApp(QtGui.QMainWindow):
         self.ui.setupUi(self)
         self.ui.mapView.setScene(QtGui.QGraphicsScene(0, 0, 1000, 1000))
         self.ui.mapView.scene().mousePressEvent = self.mouseClickedOnMap
-        self.ui.mapView_2.setScene(QtGui.QGraphicsScene(0, 0, 1000, 1000))
-        self.ui.mapView_2.scene().mousePressEvent = self.mouseClickedOnMap
+#        self.ui.mapView_2.setScene(QtGui.QGraphicsScene(0, 0, 1000, 1000))
+        self.ui.mapView_2.setScene(self.ui.mapView.scene())
+        self.ui.mapView_3.setScene(self.ui.mapView.scene())
+#        self.ui.mapView_2.scene().mousePressEvent = self.mouseClickedOnMap
         self.regions = {}
+        self.continents = {} # dict of lists
+        self.continentBonus = {} #dict of ints
+        self.continentState = 0 # 0 = initial, 1 = addRegion 2 = removeRegion
+        self.currentContinent = None
         self.chooseRegion(None)
         self.currentTabIndex = self.ui.tabWidget.currentIndex()
         self.formLinking = False
         self.removeLinking = False
         self.selectedRegion = None
+        self.imageF = None
         
     def log(self, message):
-        if(self.currentTabIndex == 0):
-            self.ui.logText.appendPlainText(message)
-        elif(self.currentTabIndex == 1):
-            self.ui.logText_2.appendPlainText(message)
+#        if(self.currentTabIndex == 0):
+        self.ui.logText.appendPlainText(message)
+        self.ui.logText_2.appendPlainText(message)
+        self.ui.logText_3.appendPlainText(message)
+#        elif(self.currentTabIndex == 1):
+#            self.ui.logText_2.appendPlainText(message)
 
     def refreshRegionList(self):
         self.ui.regionList.clear()
@@ -164,6 +175,35 @@ class MapCreatorApp(QtGui.QMainWindow):
                         self.removeNeighbour(v)
                     self.log("Selected region: %s" % v.name)
                     return
+        elif(self.currentTabIndex == 2):
+            point = event.scenePos()
+            for v in self.regions.values():
+                p = QtGui.QPolygonF()
+                p.insert(0, point)
+                point.setX(point.x() + 1)
+                p.insert(1, point)
+                point.setY(point.y() + 1)
+                p.insert(2, point)
+                i = v.polygon.intersected(p)
+                if(i.count() > 1):
+                    self.log("Clicked region: %s" % v.name)
+                    if(self.continentState == 0):
+                        self.log("Insignificant action")  
+                    elif(self.continentState == 1):
+                        if(v.continent is None):
+                            self.continents[self.currentContinent].append(v.name)
+                            v.continent = self.currentContinent
+                            self.log("%s is added in continent %s" % (v.name, v.continent))
+                        else:
+                            self.log("%s is already in continent %s" % (v.name, v.continent))
+                    else:
+                        if(v.name in self.currentContinent and v.continent == self.currentContinent):
+                            self.continents[self.currentContinent].remove(v.name)
+                            v.continent = None
+                            self.log("%s is removed from continent %s" % (v.name, self.currentContinent))
+                        else:
+                            self.log("%s is not in continent %s" % (v.name, self.currentContinent))
+                    return
 
     def addNeighbour(self, r):
         if(not (r.name in self.currentRegion.neighbours)):
@@ -197,6 +237,7 @@ class MapCreatorApp(QtGui.QMainWindow):
 
     def actionOpen(self):
         filename = QtGui.QFileDialog.getOpenFileName(self, "Open an Image")
+        self.imageF = filename
         try:
             image = QtGui.QPixmap(filename)
             item = QtGui.QGraphicsPixmapItem(image)
@@ -210,15 +251,81 @@ class MapCreatorApp(QtGui.QMainWindow):
 
     def actionSave(self):
         self.log("Clicked save menu")
+        filename = QtGui.QFileDialog.getSaveFileName(self, "Select file to save")
+        self.log("Saving to file %s" % filename)
+        if(self.imageF is None):
+            self.log("No image file")
+        else:
+            imagef = open(filename+"_image", "w")
+            copyfile(self.imageF, imagef)
+            self.log("Saved image file at %s" % imagef)
+        f = open(filename, "w")
+        f.write(str(len(self.regions))+"\n")
+        for r in self.regions.values():
+            f.write(r.name+ ",")
+            f.write(r.continent+":")
+            for n in r.neighbours:
+                f.write(n+",")
+            f.write(":")
+            for i in range(r.polygon.count()):
+                p = r.polygon.at(i)
+                f.write(str(p.x())+"-"+str(p.y())+",")
+            f.write("\n");
+        f.write(str(len(self.continents))+ "\n")
+        for c in self.continents:
+            f.write(c+","+str(self.continentBonus[c])+"\n")
+        f.flush()
+        f.close()
         
     def chooseRegion(self, r):
         self.currentRegion = r
-        if(r is None):
-            self.ui.linkButton.setEnabled(True)
+    def createContinent(self):
+        self.log("Create Continent clicked")
+        name = self.ui.continentNameEdit.text()
+        bonus = int(self.ui.continentBonusEdit.text())
+        if(name == "" or bonus <= 0 ):
+            self.log("Invalid values for continent name or bonus")
+        elif(name in self.continents):
+            self.log("Continent %s is already defined" % name)
         else:
-            self.ui.linkButton.setEnabled(False)
-    def link(self):
-        pass
+            self.log("Created continent %s %d" % (name, bonus))
+            self.continents[name] = []
+            self.continentBonus[name] = bonus
+            self.ui.continentList.addItem(name)
+    
+    def deleteContinent(self):
+        self.log("Delete Continent clicked")
+        if(self.currentContinent is None):
+            self.log("No continent selected")
+        else:
+            del self.continentBonus[self.currentContinent]
+            for regionName in self.continents[self.currentContinent]:
+                self.regions[regionName].continent = None
+            del self.continents[self.currentContinent]
+            self.ui.continentList.takeItem(self.ui.continentList.currentRow())
+            self.currentContinent = None
+    def addRegionToContinent(self):
+        self.log("Add Region To Continent clicked")
+        if(self.currentContinent is None):
+            self.log("Select a continent first")
+        else:
+            self.continentState = 1
+            self.log("Now select regions...")
+    def removeRegionFromContinent(self):
+        self.log("Remove Region From Continent clicked")
+        if(self.currentContinent is None):
+            self.log("Select a continent first")
+        else:
+            self.continentState = 2
+            self.log("Now select regions...")
+    def selectContinent(self, continent):
+        self.log("Continent Selected %s " % continent.text())
+        self.currentContinent = continent.text()
+        self.log("Regions are:")
+        for reg in self.continents[self.currentContinent]:
+            self.log("\t%s" % reg)
+        
+    
 
 
 if __name__ == "__main__":
